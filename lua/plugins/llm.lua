22c35a4033a435e8f1f3c4df2b0e7269b77504a2
agrigoryan <aram.grigoryan@gmail.com>
2025-01-22 13:32:32 -0500

    add a keymap to toggle copilot chat

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -12,6 +12,13 @@ return {
     opts = {
       -- See Configuration section for options
     },
+    config = function()
+      local chat = require 'CopilotChat'
+      chat.setup()
+      vim.keymap.set('n', '<leader>cc', function()
+        chat.toggle {}
+      end, { desc = 'Toggle Copilot Chat' })
+    end,
     -- See Commands section for default commands if you want to lazy load on them
   },
 }
